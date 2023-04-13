205bcdf5ef1acf79653deb7a214122a5f1cf0d7b
animch <nimchuk996@gmail.com>
2023-04-13 22:18:51 +1000

    add locales

--- src/init.js
+++ src/init.js
@@ -4,37 +4,41 @@ import i18next from 'i18next';
 import _ from 'lodash';
 
 
-const schema = yup.string().trim().required().url();
 
-const validate = (url, feedLinks) => {
+export default () => {
+  const initialState = {
+    form: {
+        state: 'filling',
+        error: null,
+    },
+    posts: [],
+    feeds: [],
+    seenPosts: [],
+  };
+
+  const validate = (url) => {
+    const rssList = watchedState.rss.feeds.map((feed) => feed.url);
     const schema = yup.string().trim().required().url().notOneOf(feedLinks);
     return schema.validate(url);
-};
- 
-export default () => {
-    const initialState = {
-        form: {
-            state: 'filling',
-            error: null,
-        },
-        posts: [],
-        feeds: [],
-        seenPosts: [],
-    };
+  };
 
-    const elements = {
-        form: document.querySelector('.rss-form'),
-        input: document.querySelector('#url-input'),
-        statusMessage: document.querySelector('.feedback'),
-        posts: document.querySelector('.posts'),
-        feeds: document.querySelector('.feeds'),
-        modal: document.getElementById('modal'),
-      };
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('#url-input'),
+    statusMessage: document.querySelector('.feedback'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+    modal: document.getElementById('modal'),
+    };
     
-      const i18nextInstanse = i18next.createInstance();
-      const watchedState = watchState(initialState, i18nextInstanse, elements);
+    const i18nextInstanse = i18next.createInstance();
+    const watchedState = watchState(initialState, i18nextInstanse, elements);
     
-      const TIME_STEP = 5000;
+    const TIME_STEP = 5000;
+
+    
+
+
 
 
 };
---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  traslation: {
+    status: {
+      succes: 'RSS успешно загружен',
+      url: 'Ссылка должна быть валидным URL',
+      notOneOf: 'RSS уже существует',
+      invalidRss: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+    parseError: 'Ошибка при парсинге страницы',
+    posts: {
+      title: 'Посты',
+      button: 'Просмотр',
+    },
+    feeds: {
+      title: 'Фиды',
+    },
+  },
+};
