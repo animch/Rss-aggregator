37bff42c3d533d2f521baa3169b5901c5c1261f6
animch <nimchuk996@gmail.com>
2023-05-15 23:13:33 +1000

    fexed lint

--- src/view.js
+++ src/view.js
@@ -7,25 +7,25 @@ const renderProcessForm = (input, statusMessage, formButton) => {
   formButton.setAttribute('disabled', '');
 };
 
-const renderSuccessForm = (input, statusMessage, i18nextInstance, formButton) => {
+const renderSuccessForm = (input, statusMessage, i18next, formButton) => {
   input.classList.remove('is-invalid');
   input.value = '';
   statusMessage.classList.remove('text-danger');
   statusMessage.classList.add('text-success');
-  statusMessage.textContent = i18nextInstance.t('status.success');
+  statusMessage.textContent = i18next.t('status.success');
   input.removeAttribute('readonly');
   formButton.removeAttribute('disabled');
 };
 
-const renderErrorForm = (input, statusMessage, i18nextInstance, error, formButton) => {
+const renderErrorForm = (input, statusMessage, i18next, error, formButton) => {
   input.classList.add('is-invalid');
   statusMessage.classList.add('text-danger');
-  statusMessage.textContent = i18nextInstance.t(`status.${error}`);
+  statusMessage.textContent = i18next.t(`status.${error}`);
   input.removeAttribute('readonly');
   formButton.removeAttribute('disabled');
 };
 
-const renderContainer = (type, i18nextInstance) => {
+const renderContainer = (type, i18next) => {
   const container = document.createElement('div');
   container.classList.add('card', 'border-0');
   const cardBody = document.createElement('div');
@@ -34,19 +34,19 @@ const renderContainer = (type, i18nextInstance) => {
   const header = document.createElement('h2');
   cardBody.append(header);
   header.classList.add('card-title', 'h4');
-  header.textContent = type === 'feeds' ? i18nextInstance.t('feeds.title') : i18nextInstance.t('posts.title');
+  header.textContent = type === 'feeds' ? i18next.t('feeds.title') : i18next.t('posts.title');
   const list = document.createElement('ul');
   cardBody.append(list);
   list.classList.add('list-group', 'border-0', 'rounded-0');
   return container;
 };
 
-const renderPosts = (postsEl, i18nextInstance, postList) => {
+const renderPosts = (postsEl, i18next, postList) => {
   postsEl.innerHTML = '';
   if (postList.length === 0) {
     return;
   }
-  const view = renderContainer('posts', i18nextInstance);
+  const view = renderContainer('posts', i18next);
   const list = view.querySelector('ul');
   postList.forEach((el) => {
     const post = document.createElement('li');
@@ -67,18 +67,18 @@ const renderPosts = (postsEl, i18nextInstance, postList) => {
     button.setAttribute('data-id', el.id);
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#modal');
-    button.textContent = i18nextInstance.t('posts.button');
+    button.textContent = i18next.t('posts.button');
     list.append(post);
   });
   postsEl.append(view);
 };
 
-const renderFeeds = (feedsEl, i18nextInstance, feedList) => {
+const renderFeeds = (feedsEl, i18next, feedList) => {
   feedsEl.innerHTML = '';
   if (feedList.length === 0) {
     return;
   }
-  const view = renderContainer('feeds', i18nextInstance);
+  const view = renderContainer('feeds', i18next);
   const list = view.querySelector('ul');
   feedList.forEach((el) => {
     const feed = document.createElement('li');
@@ -113,28 +113,28 @@ const renderModalWindow = (modalEl, modalState) => {
   readFullArticle.href = modalState.link;
 };
 
-export default (state, i18nextInstance, el) => onChange(state, (path, value) => {
+export default (state, i18next, el) => onChange(state, (path, value) => {
   switch (path) {
     case 'form.status':
       switch (value) {
         case 'success':
-          renderSuccessForm(el.input, el.statusMessage, i18nextInstance, el.formButton);
+          renderSuccessForm(el.input, el.statusMessage, i18next, el.formButton);
           break;
         case 'inProcess':
           renderProcessForm(el.input, el.statusMessage, el.formButton);
           break;
         case 'error':
-          renderErrorForm(el.input, el.statusMessage, i18nextInstance, state.form.error, el.formButton);
+          renderErrorForm(el.input, el.statusMessage, i18next, state.form.error, el.formButton);
           break;
         default:
           throw new Error('Unexpected form status');
       }
       break;
     case 'rss.feeds':
-      renderFeeds(el.feeds, i18nextInstance, value);
+      renderFeeds(el.feeds, i18next, value);
       break;
     case 'rss.posts':
-      renderPosts(el.posts, i18nextInstance, value);
+      renderPosts(el.posts, i18next, value);
       keepSeenPosts(state.rss.seenPosts);
       break;
     case 'rss.seenPosts':
